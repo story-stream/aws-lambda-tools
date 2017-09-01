7d2c95a79eb6d2d0c407fe8619d2e08d776a6a54
Gary Prescott <gary.prescott@storystream.it>
2017-09-01 13:14:27 +0100

    Added tests and hopefully fixed issues with live_client

--- aws_lambda_tools/clients/live_client.py
+++ aws_lambda_tools/clients/live_client.py
@@ -7,9 +7,18 @@ class Client(object):
     def __init__(self, *args, **kwargs):
         self._client = boto3.client(*args, **kwargs)
 
-    def invoke(self, *args, **kwargs):
-        response = self._client.invoke(*args, **kwargs)
-        return json.loads(''.join(_get_payload(result['Payload'])))
+    def invoke(self, FunctionName, Payload, InvocationType='Event', **kwargs):
+        if hasattr(Payload, 'iteritems'):
+            Payload = json.dumps(Payload)
+
+        response = self._client.invoke(
+            FunctionName=FunctionName,
+            Payload=Payload,
+            InvocationType=InvocationType,
+            **kwargs
+        )
+
+        return json.loads(''.join(self._get_payload(response['Payload'])))
 
     def _get_payload(self, body):
         for chunk in iter(lambda: body.read(1024), b''):
---
+++ aws_lambda_tools/tests/clients/live_client_tests.py
@@ -0,0 +1,71 @@
+import boto3
+import unittest
+from aws_lambda_tools.clients.live_client import Client
+from mock import patch, MagicMock
+
+
+class LiveClientBaseTestCase(unittest.TestCase):
+
+    @patch('aws_lambda_tools.clients.live_client.boto3.client', spec=True)
+    def test_creates_boto_client_instance_with_provided_params(self, mock_client):
+        target = Client(clive=False, dave=True)
+        client = target._client
+
+        mock_client.assert_called_once_with(clive=False, dave=True)
+
+
+class LiveClientInvokeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = MagicMock()
+        client_patch = patch('aws_lambda_tools.clients.live_client.boto3.client', return_value=self.client)
+        client_patch.start()
+        self.addCleanup(client_patch.stop)
+
+        self.target = Client()
+        self.target._client = self.client
+        self.target._get_payload = MagicMock()
+        self.target._get_payload.side_effect = lambda x: x
+
+        self.client.invoke.return_value = {
+            'Payload': '{"result": "ok"}',
+        }
+
+    def test_calls_client_invoke_with_provided_parameters(self):
+        self.target.invoke(
+            FunctionName='testing',
+            Payload='{"test": true}',
+            InvocationType='Testing',
+        )
+
+        self.client.invoke.assert_called_once_with(
+            FunctionName='testing',
+            Payload='{"test": true}',
+            InvocationType='Testing'
+        )
+
+    def test_converts_dictionary_payload_into_json(self):
+        self.target.invoke(
+            FunctionName='testing',
+            Payload={'test': True},
+            InvocationType='Testing',
+        )
+
+        self.client.invoke.assert_called_once_with(
+            FunctionName='testing',
+            Payload='{"test": true}',
+            InvocationType='Testing'
+        )
+
+    def test_returns_result_as_a_dictionary(self):
+        self.target._get_payload.return_value = '{"result": "ok"}'
+
+        expected = {'result': 'ok'}
+
+        actual = self.target.invoke(
+            FunctionName='testing',
+            Payload={'test': True},
+            InvocationType='Testing',
+        )
+
+        self.assertEqual(actual, expected)
