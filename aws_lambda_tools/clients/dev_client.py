618c1faa54908ddcd688f309d01b61fbec5c635b
Ash <ashley.smith@storystream.it>
2019-09-06 17:04:07 +0100

    updating dev client to use module name in config if provided

--- aws_lambda_tools/clients/dev_client.py
+++ aws_lambda_tools/clients/dev_client.py
@@ -1,4 +1,7 @@
+import os
 import requests
+from pydoc import locate
+
 from aws_lambda_tools import utils
 
 
@@ -8,14 +11,27 @@ class Client(object):
         pass
 
     def invoke(self, FunctionName, Payload, InvocationType='RequestResponse'):
+        FunctionName = self._parse_function_name(FunctionName)
+
         return requests.post(
             u'http://{}'.format(FunctionName),
             json=Payload
         ).json()
 
     def start_execution(self, stateMachineArn, name, input, **kwargs):
+        FunctionName = self._parse_function_name(FunctionName)
+
         f = utils.get_state_machine_name(stateMachineArn)
         return requests.post(
             u'http://{}'.format(f),
             json=input
         ).json()
+    
+    def _parse_function_name(self, FunctionName):
+        config_location = os.environ.get('APP_CONFIG')
+        if config_location:
+            config = locate(config_location)
+            if config and FunctionName.startswith(config.MODULE_NAME):
+                FunctionName = f'{MODULE_NAME}/FunctionName[len(FunctionName):]'
+
+        return FunctionName
