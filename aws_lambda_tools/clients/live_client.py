7cb7e5d16749011d61f4edc354d5d2c09e6029a7
Ash <ashley.smith@storystream.it>
2019-09-09 18:33:55 +0100

    fixing for python2 and 3

--- aws_lambda_tools/clients/live_client.py
+++ aws_lambda_tools/clients/live_client.py
@@ -8,9 +8,12 @@ class Client(object):
         self._client = boto3.client(*args, **kwargs)
 
     def invoke(self, FunctionName, Payload, InvocationType='RequestResponse', **kwargs):
+        if hasattr(Payload, 'items') or hasattr(input, 'iteritems'):
+            Payload = json.dumps(Payload, default=lambda x: x.isoformat() if hasattr(x, 'isoformat') else x)
+
         response = self._client.invoke(
             FunctionName=FunctionName,
-            Payload=json.dumps(Payload, default=lambda x: x.isoformat() if hasattr(x, 'isoformat') else x),
+            Payload=Payload,
             InvocationType=InvocationType,
             **kwargs
         )
@@ -21,10 +24,13 @@ class Client(object):
         return json.loads(''.join(self._get_payload(response['Payload'])))
 
     def start_execution(self, stateMachineArn, name, input, **kwargs):
+        if hasattr(input, 'items') or hasattr(input, 'iteritems'):
+            input = json.dumps(input, default=lambda x: x.isoformat() if hasattr(x, 'isoformat') else x)
+
         result = self._client.start_execution(
             stateMachineArn=stateMachineArn,
             name=name,
-            input=json.dumps(input, default=lambda x: x.isoformat() if hasattr(x, 'isoformat') else x),
+            input=input,
             **kwargs
         )
 
