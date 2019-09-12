74babe6043c5608d2947f755fd9f7a97cccf34ea
Ash <ashley.smith@storystream.it>
2019-09-12 20:38:21 +0100

    changing log decorator name

--- aws_lambda_tools/logging.py
+++ aws_lambda_tools/logging.py
@@ -1,21 +1,22 @@
 import logging
 from functools import wraps
 
+
 logger = logging.getLogger('lambda')
 logger.setLevel(logging.INFO)
 
-
-def logging(func):
+    
+def log(func):
 
     @wraps(func)
-    def wrapped_func(*args, **kwargs):
+    def wrapped_function(*args, **kwargs):
         logger.info('Recieved: args={} kwargs={}'.format(args, kwargs))
         
-        result = func(logger=logger, *args, **kwargs)
+        result = func(*args, **kwargs)
 
         logger.info('Returning: {}'.format(result))
 
         return result
     
-    return wrapped_func
+    return wrapped_function
     
