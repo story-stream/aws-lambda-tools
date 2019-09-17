e4c8df8d6ca2876b25c09510b97e08fb649eeecf
Ash <ashley.smith@storystream.it>
2019-09-17 14:58:32 +0100

    setting environment variable manually

--- aws_lambda_tools/tests/core/utils_tests.py
+++ aws_lambda_tools/tests/core/utils_tests.py
@@ -1,22 +1,13 @@
 import unittest
-from mock import patch
-
+from aws_lambda_tools.core import utils
 
 
 class UtilsTestCase(unittest.TestCase):
     def setUp(self):
-        os_patcher = patch('aws_lambda_tools.core.utils.os')
-        os = os_patcher.start()
-        os.environ = {
-            'AWS_REGION': 'test'
-        }
-        self.addCleanup(os_patcher.stop)
-
-        from aws_lambda_tools.core import utils
-        self.utils = utils
+        os.environ['AWS_REGION'] = 'test'
 
     def test_get_state_machine_name_returns_name_from_arn(self):
         arn = 'arn:aws:states:eu-west-1:754902810601:stateMachine:video-processing'
         expected = 'video-processing'
-        actual = self.utils.get_state_machine_name(arn)
+        actual = utils.get_state_machine_name(arn)
         self.assertEqual(actual, expected)
