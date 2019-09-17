c476c2698d25cc2a2873175d226d5547b738695e
Ash <ashley.smith@storystream.it>
2019-09-17 15:25:41 +0100

    setting environment variable in test suite

--- aws_lambda_tools/tests/core/utils_tests.py
+++ aws_lambda_tools/tests/core/utils_tests.py
@@ -1,16 +1,11 @@
-import os
 import unittest
+from aws_lambda_tools.core import utils
 
 
 class UtilsTestCase(unittest.TestCase):
-    def setUp(self):
-        os.environ['AWS_REGION'] = 'test'
-
-        from aws_lambda_tools.core import utils
-        self.utils = utils
 
     def test_get_state_machine_name_returns_name_from_arn(self):
         arn = 'arn:aws:states:eu-west-1:754902810601:stateMachine:video-processing'
         expected = 'video-processing'
-        actual = self.utils.get_state_machine_name(arn)
+        actual = utils.get_state_machine_name(arn)
         self.assertEqual(actual, expected)
