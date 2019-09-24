b9dafb7dc68e3a04a83bad3186b1b88ffbf46ef1
Ash <ashley.smith@storystream.it>
2019-09-24 14:56:58 +0100

    updating test

--- aws_lambda_tools/tests/clients/live_client_tests.py
+++ aws_lambda_tools/tests/clients/live_client_tests.py
@@ -22,7 +22,7 @@ class LiveClientInvokeTestCase(unittest.TestCase):
         client_patch.start()
         self.addCleanup(client_patch.stop)
 
-        self.target = Client()
+        self.target = Client('lambda')
         self.target._client = self.client
         self.target._get_payload = MagicMock()
         self.target._get_payload.side_effect = lambda x: x
