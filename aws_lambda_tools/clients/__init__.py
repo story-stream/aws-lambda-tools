b72c88e913f7ff4478709ad375afb56dbe0ecac0
Ash <ashley.smith@storystream.it>
2019-11-27 12:17:49 +0000

    adding error message to import error

--- aws_lambda_tools/clients/__init__.py
+++ aws_lambda_tools/clients/__init__.py
@@ -5,8 +5,9 @@ from pydoc import locate
 
 environment = os.environ.get('STYSTM_ENVIRON', 'dev')
 
+client_string = 'aws_lambda_tools.clients.{}.Client'.format(environment)
 
-Client = locate('aws_lambda_tools.clients.{}.Client'.format(environment))
+Client = locate(client_string)
 
 if not Client:
-    raise ImportError
+    raise ImportError('Environment {} is not valid. {}'.format(environment, client_string))
