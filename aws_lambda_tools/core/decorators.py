04c80ff3426388dbecd7970c7b306c9b604fa0e3
christopher.harman@storystream.it <christopher.harman@storystream.it>
2020-04-27 09:14:00 +0100

    Adding masked_log (custom output field masking) to core decorators

--- aws_lambda_tools/core/decorators.py
+++ aws_lambda_tools/core/decorators.py
@@ -40,6 +40,40 @@ def log_after(func):
     return wrapped_function
 
 
+def masked_log(masked_fields=None):
+
+    def actual_log(func):
+
+        @masked_log_after(masked_fields=masked_fields)
+        @log_before
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            return _execute(func, *args, **kwargs)
+
+        return wrapped_function
+    
+    return actual_log
+
+
+def masked_log_after(masked_fields=None):
+
+    def actual_log_after(func):
+
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            result = _execute(func, *args, **kwargs)
+
+            logged_result = _mask_output(result, masked_fields=masked_fields)
+
+            logger.info('Returning: {}'.format(json.dumps(logged_result)))
+
+            return result
+
+        return wrapped_function
+
+    return actual_log_after
+
+
 def api(func):
 
     @wraps(func)
@@ -95,3 +129,37 @@ def _execute(func, *args, **kwargs):
     except Exception:
         logger.exception('Error occurred')
         raise
+
+
+def _apply_mask_to_entry(entry, masked_fields=None):
+    return {
+        key: _apply_mask_to_value(value, key=key, masked_fields=masked_fields)
+        for key, value in entry.items()
+    }
+
+
+def _apply_mask_to_value(value, key=None, masked_fields=None):
+    if key not in masked_fields:
+        return value
+
+    return '*' * 16
+
+
+def _mask_output(result, masked_fields=None):
+    if not masked_fields:
+        return result
+
+    result_body = json.loads(result['body'])
+
+    if isinstance(result, dict):
+        # Single entry in output
+        masked_result = _apply_mask_to_entry(result_body, masked_fields=masked_fields)
+    else:
+        # Multiple entries in output
+        masked_result = [
+            _apply_mask_to_entry(entry, masked_fields=masked_fields)
+            for entry in result_body
+        ]
+
+    result['body'] = masked_result
+    return result
